958ae84f724acdfda2d986252461c2c0f0900dd3
Shuhsi Lin <shuhsilin@micron.com>
2023-12-05 15:46:53 +0000

    split customers.sql into mutiple models

--- models/customers.sql
+++ models/customers.sql
@@ -1,25 +1,6 @@
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
-),
+select * from {{ ref('stg_customers') }}
+select * from {{ ref('stg_orders') }}
+,
 
 customer_orders as (
 
